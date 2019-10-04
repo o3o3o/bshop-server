8d372393cec6817e39187c9338072b0b04ad6684
michael <tianya0yingzi@gmail.com>
2019-10-04 11:21:22 +0800

    add admin

--- bshop/user_center/admin.py
+++ bshop/user_center/admin.py
@@ -4,5 +4,5 @@ from user_center.models import ShopUser
 
 @admin.register(ShopUser)
 class ShopUserAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "phone", "created_at")
+    list_display = ("id", "user", "phone", "is_vendor", "vendor_name", "created_at")
     search_fields = ["phone", "user__username"]
